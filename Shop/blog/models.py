29b1215180b200a152fa64820cf6a4b41b461d87
iliya goodarzi <iliyagoodarzi919@gmail.com>
2023-09-01 23:35:27 -0700

    add

--- Shop/blog/models.py
+++ Shop/blog/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=50 , null=True)
+    def __str__(self):
+        return self.name
+
+
 class product(models.Model):
     ProductName = models.CharField(max_length=200)
     ShortDescription = models.CharField(max_length=100)
     Description = models.TextField()
     Data = models.DateField()
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
 
     def __str__(self):
         return self.ProductName
--- Shop/blog/views.py
+++ Shop/blog/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import product
+from .models import product , Category
 
 
 class Blog(ListView):
     template_name = 'blog.html'
     model = product
     context_object_name = 'products'
+
+class Category(ListView):
+    template_name = 'Category.html'
+    model = Category
+    context_object_name = 'Categorys'    
 
 
 class BlogDetail(DetailView):
